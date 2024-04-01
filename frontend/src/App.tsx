6249c01a94d7e3925fcc4475e52ff8d647bcccf0
Jason Gao <jgao2299@gmail.com>
2024-04-01 16:57:36 -0400

    skeleton feed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Card from "@/components/Card.tsx";
+import { Segment } from "semantic-ui-react";
+import "@/App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export type IPostingList = IPosting[];
+export interface IPosting {
+  id: number;
+  image: string,
+  username: string,
+  postedDate: string,
+  avatar: string,
+  description: string,
+  comments: IPostingComment[],
 }
 
-export default App
+export interface IPostingComment {
+  username: string,
+  comment: string
+}
+
+
+function App() {
+  const config: IPostingList = [
+    {
+      id: 1,
+      image: "https://react.semantic-ui.com/images/avatar/large/matthew.png",
+      username: "Jason",
+      postedDate: "1h ago",
+      avatar: "https://react.semantic-ui.com/images/avatar/small/elliot.jpg",
+      description: "Wedding photo #1",
+      comments: [
+        {
+          username: "Jason",
+          comment: "Long comment made by a bot that is meant to span multiple lines"
+        },
+        {
+          username: "Jason",
+          comment: "Long comment made by a bot that is meant to span multiple lines"
+        }
+      ]
+    },
+    {
+      id: 2,
+      image: "https://react.semantic-ui.com/images/avatar/large/matthew.png",
+      username: "Jason",
+      postedDate: "1h ago",
+      avatar: "https://react.semantic-ui.com/images/avatar/small/elliot.jpg",
+      description: "Wedding photo #2",
+      comments: [
+        {
+          username: "Jason",
+          comment: "Long comment made by a bot that is meant to span multiple lines"
+        },
+        {
+          username: "Jason",
+          comment: "Long comment made by a bot that is meant to span multiple lines"
+        }
+      ]
+    },
+  ];
+  return (
+    <>
+      <div className="app-container">
+        <Segment className="over">
+          {
+            config.map(config => (
+              <Card config={config}/>
+            ))
+          }
+        </Segment>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Card.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import {
+  CardMeta,
+  CardHeader,
+  CardDescription,
+  CardContent,
+  Card,
+  Icon,
+  Image,
+} from "semantic-ui-react";
+import "@/components/Card.css";
+
+import { IPosting } from "../App";
+
+interface IProps {
+  config: IPosting;
+}
+
+const CustomCard = ({ config }: IProps) => {
+  return (
+    <Card fluid>
+      <CardContent>
+        <CardHeader>
+          <Image src={config.avatar} avatar className="float-left" />
+          <span className="float-left mt-2">{config.username}</span>
+          <span className="float-right mt-2 font-weight-normal">
+            {config.postedDate}
+          </span>
+        </CardHeader>
+      </CardContent>
+      <Image src={config.image} wrapped ui={false} />
+      <CardContent>
+        <Icon size="big" name="heart outline" />
+        <Icon size="big" name="comment outline" />
+        <CardDescription>{config.description}</CardDescription>
+      </CardContent>
+      <CardContent>
+        {config.comments.map((comment) => (
+          <CardDescription>
+            <span className="font-weight-bold">{comment.username}</span> {comment.comment}
+          </CardDescription>
+        ))}
+        <CardMeta>View {config.comments.length} comments</CardMeta>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default CustomCard;
